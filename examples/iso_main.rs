b0f0ac60dd13266a2d3cce730b216b1794c973a2
Adam Shaw <adam.shaw@slalom.com>
2020-09-28 11:49:22 -0400

    Fix ISO center as well

--- examples/iso_main.rs
+++ examples/iso_main.rs
@@ -1,4 +1,5 @@
 use bevy::{prelude::*, render::camera::Camera};
+use bevy_tiled::TiledMapCenter;
 
 fn main() {
     App::build()
@@ -13,7 +14,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
         .spawn(bevy_tiled::TiledMapComponents {
             map_asset: asset_server.load("assets/iso-map.tmx").unwrap(),
-            center: true,
+            center: TiledMapCenter(true),
             ..Default::default()
         })
         .spawn(Camera2dComponents::default());
